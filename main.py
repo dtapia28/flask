74b47308229cac88a15515513b0bce2ea082252c
Daniel Tapia <dtapia1025@gmail.com>
2025-11-09 22:42:05 -0300

    añade la tabla de usuarios al modelo de usuario

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ login_manager.login_message_category = "info"
 
 # ---------- MODELO DE USUARIO ----------
 class User(db.Model, UserMixin):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
